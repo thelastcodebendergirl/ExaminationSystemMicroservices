67961695e9cebe440ddc464bc44d918af809db1c
hdurmaz <hdurmaz2016@gmail.com>
2021-06-22 01:01:24 +0300

    examination and user services are added.

---
+++ examination-service/src/test/java/com/examinationsystemmicroservices/examinationservice/examinationservice/ExaminationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examinationsystemmicroservices.examinationservice.examinationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
